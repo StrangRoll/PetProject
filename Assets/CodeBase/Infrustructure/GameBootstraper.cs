593110db19ec5895a3cc0aa6653ff13f8f0d1d52
StrangRoll <strrranger1@gmail.com>
2025-02-01 01:58:06 +0300

    remove unnecessary files add game bootstraper

---
+++ Assets/CodeBase/Infrustructure/GameBootstraper.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GameBootstraper : MonoBehaviour
+{
+    private Game _game;
+    
+    private void Awake()
+    {
+        _game = new Game(); 
+        
+        DontDestroyOnLoad(this);
+    }
+}
