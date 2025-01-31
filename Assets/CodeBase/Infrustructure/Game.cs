8d0a577df154667d248e31ff9ea6ca1a315d1854
StrangRoll <strrranger1@gmail.com>
2025-02-01 02:25:20 +0300

    add input service

--- Assets/CodeBase/Infrustructure/Game.cs
+++ Assets/CodeBase/Infrustructure/Game.cs
@@ -1,5 +1,20 @@
+using CodeBase.Services.Input;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static IInputService InputService;
+
+    public Game()
+    {
+        RegisterInputService();
+    }
+
+    private void RegisterInputService()
+    {
+        if (Application.isEditor)
+            InputService = new StandaloneInputService();
+        else
+            InputService = new MobileInputService();
+    }
 }
