12f496863b22f99dcf3e78c2887e4a7636cc4c00
StrangRoll <strrranger1@gmail.com>
2025-03-06 14:39:19 +0300

    add hero death

---
+++ Assets/CodeBase/Hero/HeroDeath.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    [RequireComponent(typeof(HeroHealth))]
+    public class HeroDeath : MonoBehaviour
+    {
+        [SerializeField] private HeroHealth _health;
+        [SerializeField] private HeroMove _move;
+        [SerializeField] private HeroAnimator _animator;
+        [SerializeField] private ParticleSystem _deathFX;
+        
+        private bool _isDead = false;
+        
+        private void OnEnable() => 
+            _health.HealthChanged += OnHealthChanged;
+
+        private void OnDisable() => 
+            _health.HealthChanged -= OnHealthChanged;
+
+        private void OnHealthChanged(float arg1, float arg2)
+        {
+            if (_isDead == false && _health.Currrent <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            _isDead = true;
+            _move.enabled = false;
+            _animator.PlayDeath();
+            
+            _deathFX.Play();
+        }
+    }
+}
